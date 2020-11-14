c5375723ec4774f128db3e0b93ba449b5d89b8c1
jbaxe2 <jbaxe2@gmail.com>
2020-11-13 19:01:46 -0500

    minor work on workflow and plato cap components

--- lib/src/plato_cap_component.dart
+++ lib/src/plato_cap_component.dart
@@ -2,6 +2,8 @@ library plato.cap.component;
 
 import 'package:angular/angular.dart';
 import 'package:angular_components/angular_components.dart';
+import 'package:plato.cap/src/_application/error/error_component.dart';
+import 'package:plato.cap/src/_application/progress/progress_component.dart';
 
 import 'package:plato.cap/src/_application/workflow/workflow_component.dart';
 
@@ -10,7 +12,9 @@ import 'package:plato.cap/src/_application/workflow/workflow_component.dart';
   selector: 'plato-cap',
   templateUrl: 'plato_cap_component.html',
   styleUrls: ['plato_cap_component.css'],
-  directives: [WorkflowComponent],
+  directives: [
+    ErrorComponent, ProgressComponent, WorkflowComponent
+  ],
   providers: [materialProviders]
 )
 class PlatoCapComponent {
